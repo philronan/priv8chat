8fd135f4b1ea2891621f67a344295f4953ab4e10
phil_ronan <ronan@ruletheweb.co.uk>
2018-10-12 12:58:53 +0100

    revised schema

--- www/db/connect.php
+++ www/db/connect.php
@@ -1,18 +1,47 @@
 <?php
-/*
-$SECRET_KEYS = parse_ini_file("$ROOT/._keys.cfg");
 
-$mysqli = new mysqli("localhost", "sec_chat_admin", $SECRET_KEYS["MYSQL_PASSWORD"], "sec_chat");
+
+// Key scheduling, etc.
+
+// Fetch values of DBNAME, DBUSER, DBPASS and MASTERKEY
+$SECRET_KEYS = parse_ini_file("$ROOT/.config");
+
+$GLOBALS['DBNAME'] = $SECRET_KEYS['DBNAME'];
+$GLOBALS['DBUSER'] = $SECRET_KEYS['DBUSER'];
+$GLOBALS['DBPASS'] = $SECRET_KEYS['DBPASS'];
+
+// Use the master key to generate other keys/salt values
+$key = hex2bin($SECRET_KEYS['MASTERKEY']);
+$username_key = openssl_encrypt('usernameusername', 'aes-128-ecb', $key, OPENSSL_RAW_DATA | OPENSSL_ZERO_PADDING);
+$message_key  = openssl_encrypt('messagemessageme', 'aes-128-ecb', $key, OPENSSL_RAW_DATA | OPENSSL_ZERO_PADDING);
+$email_salt   = openssl_encrypt('emailsaltemailsa', 'aes-128-ecb', $key, OPENSSL_RAW_DATA | OPENSSL_ZERO_PADDING);
+$nonce_key    = openssl_encrypt('noncenoncenoncen', 'aes-128-ecb', $key, OPENSSL_RAW_DATA | OPENSSL_ZERO_PADDING);
+
+
+$GLOBALS['USERNAME_KEY'] = $username_key;
+$GLOBALS['MESSAGE_KEY'] = $message_key;
+// convert this one to ASCII because it is concatenated to the email address
+$GLOBALS['EMAIL_SALT'] = base64_encode($email_salt);
+$GLOBALS['NONCE_KEY'] = $nonce_key;
+
+// Remove the master key from memory. Not sure this is necessary, but anyway...
+unset($key, $SECRET_KEYS);
+
+
+
+// Connect to database
+
+$mysqli = new mysqli('localhost', $GLOBALS['DBUSER'], $GLOBALS['DBPASS'], $GLOBALS['DBNAME']);
 if ($mysqli->connect_errno) {
-    die("Failed to connect to MySQL: " . $mysqli->connect_error);
+    die('Failed to connect to MySQL: ' . $mysqli->connect_error);
 }
 
-ini_set("session.name", "SecChat");
-ini_set("session.cookie_lifetime", 3600 * 24 * 60);
-ini_set("session.use_only_cookies", true);
-ini_set("session.cookie_httponly", true);
+ini_set('session.name', 'SecChat');
+ini_set('session.cookie_lifetime', 3600 * 24 * 60);
+ini_set('session.use_only_cookies', true);
+ini_set('session.cookie_httponly', true);
 if (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') {
-    ini_set("session.cookie_secure", true);
+    ini_set('session.cookie_secure', true);
 }
 
 session_start();
@@ -25,4 +54,3 @@ else {
     $_SESSION['tick_count'] += 1;
     $_SESSION['last_active'] = time();
 }
-*/
--- www/schema.sql
+++ www/schema.sql
@@ -1,39 +1,33 @@
-DROP DATABASE IF EXISTS sec_chat;
-CREATE DATABASE sec_chat DEFAULT CHARACTER SET latin1 COLLATE latin1_general_ci;
+DROP TABLE IF EXISTS priv8users;
+DROP TABLE IF EXISTS priv8messages;
+DROP TABLE IF EXISTS priv8nonce;
 
-USE sec_chat;
-GRANT ALL ON sec_chat.* TO sec_chat_admin@localhost IDENTIFIED BY "0a5d47523a01087731c2c14bdb7155b4";
-GRANT LOCK TABLES, SELECT ON sec_chat.* TO sec_chat_read_only@localhost IDENTIFIED BY "hunter2";
-FLUSH PRIVILEGES;
-
-CREATE TABLE users (
+CREATE TABLE priv8users (
     user_id             INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    username_encrypted  TINYTEXT NOT NULL,
-    email               VARCHAR(160) NOT NULL UNIQUE KEY,
+    username_encrypted  VARCHAR(256) NOT NULL UNIQUE,
+    email_hashed        VARCHAR(160) NOT NULL UNIQUE KEY,
     password            VARCHAR(72) NOT NULL,
-    conf_secret         CHAR(32) NOT NULL,
-    conf_link_sent      DATETIME DEFAULT NULL,
-    conf_link_clicked   DATETIME DEFAULT NULL,
-    is_registered       TINYINT NOT NULL DEFAULT 0,
-    last_active         TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
-CREATE INDEX hash_prefix ON users (username_hash(8));
+    conf_link_sent      TIMESTAMP NOT NULL DEFAULT 0,
+    conf_link_clicked   TIMESTAMP NOT NULL DEFAULT 0,
+    is_registered       BOOLEAN NOT NULL DEFAULT FALSE
+) DEFAULT CHARACTER SET ascii;
 
-CREATE TABLE messages (
-    message_id          INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE priv8messages (
+    message_id          BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     from_user           INT NOT NULL,
     to_user             INT NOT NULL,
-    when_sent           TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    message_text        TEXT NOT NULL,
-    has_been_read       TINYINT NOT NULL DEFAULT 0,
-    sender_deleted      TINYINT NOT NULL DEFAULT 0,
-    receiver_deleted    TINYINT NOT NULL DEFAULT 0
-);
+    when_sent           TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    message_text        LONGTEXT NOT NULL,
+    has_been_read       BOOLEAN NOT NULL DEFAULT FALSE,
+    sender_deleted      BOOLEAN NOT NULL DEFAULT FALSE,
+    receiver_deleted    BOOLEAN NOT NULL DEFAULT FALSE
+) DEFAULT CHARACTER SET ascii;
+ALTER TABLE priv8messages ADD INDEX from_user (from_user);
+ALTER TABLE priv8messages ADD INDEX to_user (to_user);
+ALTER TABLE priv8messages ADD INDEX when_sent (when_sent);
 
-CREATE TABLE sessions (
-    session_token       CHAR(32) NOT NULL PRIMARY KEY,
-    ipv4                INT UNSIGNED NOT NULL,
-    ajax_name_lookup    VARCHAR(64),
-    started             TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    last_active         TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+CREATE TABLE priv8nonce (
+    id                  BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    created             TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+ALTER TABLE priv8nonce ADD INDEX created (created);
