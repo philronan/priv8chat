d8d52d509504360b84249551b656860c30cfdba5
phil_ronan <ronan@ruletheweb.co.uk>
2018-10-13 01:13:40 +0100

    Crammed the mysql schema into the setup script. Not ideal, but I'm in a hurry

--- www/pages/setup.php
+++ www/pages/setup.php
@@ -89,6 +89,43 @@ while ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // Not particularly bothered if this fails, so don't check for errors:
     @$mysqli->query("DROP TABLE `$tmp_table`;");
 
+    // Now create the database
+    @$mysqli->query("DROP TABLE IF EXISTS priv8users;");
+    @$mysqli->query("DROP TABLE IF EXISTS priv8messages;");
+    @$mysqli->query("DROP TABLE IF EXISTS priv8nonce;");
+
+    @$mysqli->query("CREATE TABLE priv8users (" .
+        "user_id             INT NOT NULL AUTO_INCREMENT PRIMARY KEY," .
+        "username_encrypted  VARCHAR(256) NOT NULL UNIQUE," .
+        "email_hashed        VARCHAR(160) NOT NULL UNIQUE KEY," .
+        "password            VARCHAR(72) NOT NULL," .
+        "conf_link_sent      TIMESTAMP NOT NULL DEFAULT 0," .
+        "conf_link_clicked   TIMESTAMP NOT NULL DEFAULT 0," .
+        "is_registered       BOOLEAN NOT NULL DEFAULT FALSE" .
+    ") DEFAULT CHARACTER SET ascii;");
+
+    @$mysqli->query("CREATE TABLE priv8messages (" .
+        "message_id          BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY," .
+        "from_user           INT NOT NULL," .
+        "to_user             INT NOT NULL," .
+        "when_sent           TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP," .
+        "message_subject     TEXT NOT NULL," .
+        "message_text        LONGTEXT NOT NULL," .
+        "has_been_read       BOOLEAN NOT NULL DEFAULT FALSE," .
+        "sender_deleted      BOOLEAN NOT NULL DEFAULT FALSE," .
+        "receiver_deleted    BOOLEAN NOT NULL DEFAULT FALSE" .
+    ") DEFAULT CHARACTER SET ascii;");
+    @$mysqli->query("ALTER TABLE priv8messages ADD INDEX from_user (from_user);");
+    @$mysqli->query("ALTER TABLE priv8messages ADD INDEX to_user (to_user);");
+    @$mysqli->query("ALTER TABLE priv8messages ADD INDEX when_sent (when_sent);");
+
+    @$mysqli->query("CREATE TABLE priv8nonce (" .
+        "id                  BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY," .
+        "created             TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP" .
+    );");
+    @$mysqli->query("ALTER TABLE priv8nonce ADD INDEX created (created);");
+
+
 
     // OK, the MySQL account is working. Now we just need to store these
     // credentials in a .config file...
@@ -120,7 +157,6 @@ while ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
 
 
-    // Success!
     // Redirect to home page
     header("HTTP/1.1 303 See Other");
     header("Location: https://" . $_SERVER['HTTP_HOST'] . "/");
